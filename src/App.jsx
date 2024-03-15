82f94c2b8030de9d45efd15b42faff3defb999f5
Drin <aldrinisit3196@gmail.com>
2024-03-15 21:25:07 +0800

    Add: split player components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./components/Player"
 
 function App() {
   return (
@@ -5,17 +6,11 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Player name="Player-1" symbol="X" />
+          <Player name="Player-2" symbol="0" />
         </ol>
 
-    
+
 
       </div>
     </main>
